1b16dcefa37e9eb39b3ed38ccfa2e69c78b998f1
Irakli-log <iraklibubu75@gmail.com>
2024-10-31 19:44:34 +0400

    second commit

---
+++ level07/homework/hw5.py
@@ -0,0 +1,10 @@
+# გაიხსენეთ type ფუნქცია და განახორციელეთ გარდაქმნები მონაცემთა ტიპებისა ცვლადში
+
+
+name = "irakli"
+age = 18
+weight = 65.5
+
+print(type(name))
+print(type(age))
+print(type(weight))
