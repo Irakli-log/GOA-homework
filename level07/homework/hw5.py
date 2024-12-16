39e04a482a464567d0c844b8d143f1e124d0ed8a
Irakli-log <iraklibubu30@gmail.com>
2024-12-17 01:18:50 +0400

    second commit

--- level07/homework/hw5.py
+++ level07/homework/hw5.py
@@ -1,10 +1,33 @@
 # გაიხსენეთ type ფუნქცია და განახორციელეთ გარდაქმნები მონაცემთა ტიპებისა ცვლადში
+ 
+# type ფუნქცია
 
+x = 56
+print(type(x)) 
 
-name = "irakli"
-age = 18
-weight = 65.5
+y = "Hello"
+print(type(y))  
 
-print(type(name))
-print(type(age))
-print(type(weight))
+# int ფუნქცია
+
+x1 = "22"
+x2 = int(x)
+print(x2)  
+print(type(x2))  
+
+# float ფუნქცია
+
+x = "5.67"
+x_float = float(x)
+print(x_float)  
+print(type(x_float))  
+
+# boolean  ფუნქცია
+
+x = 0
+x1 = bool(x)
+print(x1)
+
+y1 = "Hello"
+y1= bool(y)
+print(y1)
